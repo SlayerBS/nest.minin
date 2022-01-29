05f33d6e0f4f4c7903ac77b3589bf37e4096f0cc
Sergii Bogush <sergbogush@gmail.com>
2022-01-29 22:51:29 +0200

    add products.service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,31 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Response, Request } from 'express';
 
 @Controller('products')
 export class ProductsController {
+  //   @Get()
+  //   //   @Redirect('https://google.com', 301)
+  //   getAll(@Req() req: Request, @Res() res: Response): string {
+  //     res.status(201).end('Poka');
+  //     return 'getAll';
+  //   }
+
   @Get()
   getAll(): string {
     return 'getAll';
@@ -14,6 +37,8 @@ export class ProductsController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
   create(@Body() createPruductDto: CreateProductDto): string {
     return `Title: ${createPruductDto.title}Price:${createPruductDto.price}`;
   }
@@ -21,5 +46,10 @@ export class ProductsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return 'Remove' + id;
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return 'Update' + id;
   }
 }

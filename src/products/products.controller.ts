e6a156e3ee2cd36f0037e2303d01a3cfcc465c62
Sergii Bogush <sergbogush@gmail.com>
2022-01-29 21:45:16 +0200

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getAll() {
     return 'getAll';
+  }
+  @Get(':id')
+  getOne(@Param() params) {
+    return 'getOne' + params.id;
   }
 }

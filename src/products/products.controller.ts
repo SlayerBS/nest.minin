1d6bfc11e7c1a076466a103b2ae4ac5f69d2775a
Sergii Bogush <sergbogush@gmail.com>
2022-01-29 22:08:49 +0200

    add Delete

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getAll() {
+  getAll(): string {
     return 'getAll';
   }
+
   @Get(':id')
-  getOne(@Param() params) {
-    return 'getOne' + params.id;
+  getOne(@Param('id') id: string) {
+    return 'getOne' + id;
+  }
+
+  @Post()
+  create(@Body() createPruductDto: CreateProductDto): string {
+    return `Title: ${createPruductDto.title}Price:${createPruductDto.price}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return 'Remove' + id;
   }
 }

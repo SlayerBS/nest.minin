a182b4d751e1465bb48d8351f78da5bdfe8f9bce
Sergii Bogush <sergbogush@gmail.com>
2022-01-30 18:58:58 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(process.env.URI_DB)],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.5xbjw.mongodb.net/minin?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-
+export class AppModule {}
